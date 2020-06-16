61deea224432b61c645c6a7d5ef2ce992a947964
Roodney Moraes <roodney@houpa.app>
2020-06-16 19:54:34 -0300

    Repositorios

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Auth } from 'aws-amplify';
+
+
 function App() {
+  async function checkUser() {
+    const user = await Auth.currentAuthenticatedUser();
+    console.log("User", user);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => Auth.federatedSignIn({
+          provider: 'Facebook'
+        })}>Login com o Facebook</button>
+        <button onClick={() => Auth.federatedSignIn()}>Login</button>
+        <button onClick={checkUser}>Dados do usuario</button>
       </header>
     </div>
   );
